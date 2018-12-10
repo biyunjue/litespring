c157aac4e46a8e7bb1cc06747d41621e6410f3c4
biyunjue <biyunfengjue@gmail.com>
2018-12-10 23:59:53 +0800

    准备set注入

---
+++ src/test/java/org/litespring/dao/v2/AccountDao.java
@@ -0,0 +1,9 @@
+package org.litespring.dao.v2;
+
+/**
+ * @author yunfy
+ * @create 2018-12-10 22:43
+ **/
+public class AccountDao {
+
+}
---
+++ src/test/java/org/litespring/test/v2/ApplicationContextTestV2.java
@@ -0,0 +1,24 @@
+package org.litespring.test.v2;
+
+import org.junit.Test;
+import org.litespring.context.ApplicationContext;
+import org.litespring.context.support.ClassPathXmlApplicationContext;
+import org.litespring.service.v2.PetStoreService;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author yunfy
+ * @create 2018-12-10 23:51
+ **/
+public class ApplicationContextTestV2 {
+    @Test
+    public void testGetBeanProperty() {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("petstore-v2.xml");
+        PetStoreService petStoreService = (PetStoreService) ctx.getBean("petStore");
+
+        assertNotNull(petStoreService.getAccountDao());
+        assertNotNull(petStoreService.getItemDao());
+
+    }
+}
